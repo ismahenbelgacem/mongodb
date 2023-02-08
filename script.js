e8c6397a89e48c05396ec37f0ad03061095cba64
ismahenbelgacem <114505887+ismahenbelgacem@users.noreply.github.com>
2023-02-08 20:22:46 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+const contactlist = [
+    {
+      lastName: "Ben Lahmer",
+      firstName: "Fares",
+      email: "fares@gmail.com",
+      age: 26,
+    },
+    {
+      lastName: "Kefi",
+      firstName: "Seif",
+      email: "kefi@gmail.com",
+      age: 15,
+    },
+    {
+      lastName: "Fantassi",
+      firstName: "Sarra",
+      email: "sarra.f@gmail.com",
+      age: 40,
+    },
+    {
+      lastName: "Ben Yehia",
+      firstName: "Rym",
+      age: 4,
+    },
+    {
+      lastName: "Cherif",
+      firstName: "Sami",
+      age: 3,
+    },
+  ];
